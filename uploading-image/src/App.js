f5130329cf2d0be8b6ab2fc8df57f9ddd29caa49
HARSH CHATURVEDI <harshchaturvedi589@gmail.com>
2021-12-08 15:28:01 +0000

    old workspace

--- uploading-image/src/App.js
+++ uploading-image/src/App.js
@@ -2,21 +2,12 @@ import './App.css';
 import React from "react";
 
 function App() {
-  const [file,setfile] = React.useState("")
-  const [text,settext] = React.useState("")
-
-  function handlesubmit(){
-    console.log("file",file)
-    console.log("text",text)
-  }
 
   return (
     <div className="App">
-      <input type="text" onChange={(e)=>settext(e.target.value)}/>
-      <input type="file" onChange={(e)=>setfile(e.target.value)}/>
-      <button onClick={()=>handlesubmit()}>Submit</button>
+      
     </div>
-  );
+ );
 }
 
 export default App;
